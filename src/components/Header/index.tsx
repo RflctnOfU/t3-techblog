5f80c0611368fce56fb18f34f347c66729919497
Kris Marshall <rflctnofu@hotmail.com>
2023-05-15 09:36:50 -0400

    header and layout done before last commit, adjusted padding in header and modified welcome message a bit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   const { data: sessionData } = useSession();
   const userName = sessionData?.user.name;
   return (
-    <header className="font-['Work Sans'] flex items-center justify-between bg-gradient-to-r from-slate-700 from-10% via-blue-500 via-40% to-indigo-400 to-90% p-6">
+    <header className="font-['Work Sans'] flex items-center justify-between bg-gradient-to-r from-slate-700 from-10% via-blue-500 via-40% to-indigo-400 to-90% px-8 py-6">
       <div className="px-4 py-2">
         <div className="border-none shadow-lg">
           <div className="rounded-md border-2 border-indigo-800 bg-gradient-to-br from-violet-700 via-indigo-700 to-blue-700 p-2 px-4 pb-4 text-4xl text-indigo-300 shadow-inner shadow-blue-500">
@@ -22,14 +22,16 @@ const Header = () => {
           </div>
         </div>
       </div>
-      {sessionData && (
-        <div
-          style={titil.style}
-          className="bg-gradient-to-r from-slate-700 via-neutral-700 to-gray-700 bg-clip-text text-xl text-transparent"
-        >
-          Welcome Back {userName}
-        </div>
-      )}
+
+      <div
+        style={titil.style}
+        className="bg-gradient-to-r from-slate-700 via-neutral-700 to-gray-700 bg-clip-text text-xl text-transparent"
+      >
+        {sessionData
+          ? `Welcome Back ${userName as string}`
+          : "Welcome to the Tech Blog"}
+      </div>
+
       <div className="flex justify-between gap-2 px-4 py-2">
         <Link href={"/"}>
           <Button style={titil.style} className="nav-button">
